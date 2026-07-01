a73f5071502b485713cca020245c863ce3e06faf
aimartinvega-code <aimartinvega@gmail.com>
2026-07-01 09:14:20 -0300

    fix monto original no se pisa con facturas

--- app.py
+++ app.py
@@ -233,9 +233,13 @@ def get_clientes():
         facturas = data.get('facturas', {}).get(str(c['id']), [])
         c2['facturas'] = facturas
         if facturas:
-            mo, int_ = calcular_totales_cliente({'facturas': facturas}, tasa)
-            c2['monto_original'] = mo
-            c2['intereses'] = int_
+            # Suma facturas + monto_original manual (si existe)
+            mo_facturas = sum(f.get('monto', 0) or 0 for f in facturas)
+            int_facturas = sum(calcular_interes_factura(f.get('monto', 0), f.get('fecha_mora', ''), tasa) for f in facturas)
+            mo_manual = c.get('monto_original', 0) or 0
+            # Si hay monto manual sin factura asociada, lo sumamos
+            c2['monto_original'] = mo_facturas + mo_manual
+            c2['intereses'] = int_facturas
         clientes.append(c2)
     return jsonify(clientes)
 
@@ -341,14 +345,6 @@ def add_factura(cid):
     if str(cid) not in data.get('facturas', {}):
         data.setdefault('facturas', {})[str(cid)] = []
     data['facturas'][str(cid)].append(factura)
-    # Recalcular totales del cliente
-    facturas = data['facturas'][str(cid)]
-    mo, int_ = calcular_totales_cliente({'facturas': facturas}, tasa)
-    for c in data['clientes']:
-        if c['id'] == cid:
-            c['monto_original'] = mo
-            c['intereses'] = int_
-            break
     save_data(data)
     factura['interes_calculado'] = calcular_interes_factura(factura['monto'], factura['fecha_mora'], tasa)
     factura['total'] = factura['monto'] + factura['interes_calculado']
@@ -361,13 +357,6 @@ def delete_factura(cid, fid):
     tasa = data.get('tasa_bna', 60.0)
     facturas = data.get('facturas', {}).get(str(cid), [])
     data['facturas'][str(cid)] = [f for f in facturas if f.get('id') != fid]
-    facturas_new = data['facturas'][str(cid)]
-    mo, int_ = calcular_totales_cliente({'facturas': facturas_new}, tasa)
-    for c in data['clientes']:
-        if c['id'] == cid:
-            c['monto_original'] = mo
-            c['intereses'] = int_
-            break
     save_data(data)
     return jsonify({'ok': True})
 
