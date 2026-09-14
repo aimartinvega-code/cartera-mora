5935611cfdd2e6d627a265a93f1d474d26dc8cac
aimartinvega-code <aimartinvega@gmail.com>
2026-09-14 18:15:07 -0300

    fix indentation cheque resumen

--- app.py
+++ app.py
@@ -660,18 +660,18 @@ def get_resumen():
                         pagado += p.get('monto', 0) or 0
                     elif p.get('tipo') == 'cheque':
                         if p.get('cheque_rechazado') or p.get('cheque_estado') == 'rechazado':
-                                pass
-                            elif p.get('cheque_cobrado') or p.get('cheque_estado') == 'cobrado':
-                                pagado += p.get('monto', 0) or 0
-                            else:
-                                try:
-                                    fc = datetime.strptime(p.get('fecha_cobro_cheque', ''), '%Y-%m-%d').date()
-                                    if fc <= hoy:
-                                        pagado += p.get('monto', 0) or 0
-                                    else:
-                                        cheque_pend += p.get('monto', 0) or 0
-                        except:
                             pass
+                        elif p.get('cheque_cobrado') or p.get('cheque_estado') == 'cobrado':
+                            pagado += p.get('monto', 0) or 0
+                        else:
+                            try:
+                                fc = datetime.strptime(p.get('fecha_cobro_cheque', ''), '%Y-%m-%d').date()
+                                if fc <= hoy:
+                                    pagado += p.get('monto', 0) or 0
+                                else:
+                                    cheque_pend += p.get('monto', 0) or 0
+                            except:
+                                pass
 
             monto_total += mo
             adeudado_neto = max(0, (mo + int_) - pagado)
